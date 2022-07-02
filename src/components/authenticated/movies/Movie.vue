fdcfd0eb9c547c827d2918ee14c650e935af8dc1
luka <lukakurdadze2@gmail.com>
2022-07-02 20:08:04 +0400

    style: add grid to movies page

--- src/components/authenticated/movies/Movie.vue
+++ src/components/authenticated/movies/Movie.vue
@@ -1,15 +1,15 @@
 <template>
   <div
-    class="flex flex-col gap-5 w-full max-w-4xl p-10 text-white lg:rounded-[10px]"
+    class="flex flex-col gap-5 w-full max-w-4xl p-10 text-white lg:rounded-[10px] md:pb-0"
   >
     <div class="flex flex-col gap-5">
       <img
-        class="w-full aspect-square sm:aspect-video"
+        class="w-full aspect-video lg:aspect-square"
         src="@/assets/post/post-image.png"
         alt="post-image"
       />
       <div class="flex flex-col gap-4">
-        <h1 class="text-2xl">Loki Mobius (2021)</h1>
+        <h1 class="text-2xl md:text-base">Loki Mobius (2021)</h1>
         <p class="flex gap-3">
           10 <img src="@/assets/icons/chat-quote.svg" alt="quotes" />
         </p>
