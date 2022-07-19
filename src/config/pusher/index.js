ef7514a344325f482718937e85583737adfeeac2
luka <lukakurdadze2@gmail.com>
2022-07-20 03:20:50 +0400

    feat: make pusher notification private

--- src/config/pusher/index.js
+++ src/config/pusher/index.js
@@ -1,15 +1,22 @@
 import Echo from "laravel-echo";
-
 import Pusher from "pusher-js";
+import { token } from "@/config/axios";
+
 window.Pusher = Pusher;
+Pusher.logToConsole = true;
 
 window.Echo = new Echo({
-  broadcaster: "pusher",
+  broadcaster: 'pusher',
   key: import.meta.env.VITE_WEBSOCKETS_KEY,
-  //
-  wsHost: "localhost",
-  wsPort: 6001,
-  forceTLS: false,
-  disableStats: true,
+  authEndpoint: import.meta.env.VITE_API_BASE_URL + "broadcasting/auth",
+  auth: {
+    headers: {
+      Authorization: "Bearer " + token,
+      'X-CSRF-Token': "CSRF_TOKEN",
+      "Access-Control-Allow-Origin": "*",
+      "X-Requested-With": "XMLHttpRequest",
+    }
+  },
   cluster: "eu",
+  forceTLS: true,
 });
